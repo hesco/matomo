4b191ca7b900440b8a09b93765c0cacfe3ef9d62
Alex Kisakye <kisakye@gmail.com>
2019-01-29 16:23:36 +0000

    puppet validate edits

--- manifests/mysql/db.pp
+++ manifests/mysql/db.pp
@@ -19,21 +19,21 @@ class matomo::mysql::db (
 ) {
 
 class { '::mysql::server':
-  root_password                         => $root_password,
-  remove_default_accounts               => $remove_default_accounts,
-  override_options => {
+  root_password           => $root_password,
+  remove_default_accounts => $remove_default_accounts,
+  override_options        => {
         mysqld => {
         max_allowed_packet => '64M',
             },
-	}
+        }
     }
     mysql::db { $matomo_db_name:
-        user                            => $matomo_db_user,
-        password                        => $matomo_db_password,
-        host                            => $host,
-        grant                           => $grant,
-        sql                             => $sql_back_file,
-        enforce_sql                     => $enforce_sql,
+        user        => $matomo_db_user,
+        password    => $matomo_db_password,
+        host        => $host,
+        grant       => $grant,
+        sql         => $sql_back_file,
+        enforce_sql => $enforce_sql,
     }
 
 }
